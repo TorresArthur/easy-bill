453980339a295db3ae853aad7d0d5a55741e78d3
Arthur <arthur.torres@oobj.com.br>
2022-05-05 18:18:48 -0300

    Segundo commit

--- src/main/java/br/com/alura/easybill/easybill/ProdutoRepository.java
+++ src/main/java/br/com/alura/easybill/easybill/ProdutoRepository.java
@@ -1,2 +1,71 @@
-package br.com.alura.easybill.easybill;public class ProdutoRepository {
+package br.com.alura.easybill.easybill;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class ProdutoRepository {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String urlImagem;
+    private String descricao;
+    private BigDecimal preco;
+    private BigDecimal precoPromocional;
+    private String classeFiscal;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public BigDecimal getPrecoPromocional() {
+        return precoPromocional;
+    }
+
+    public void setPrecoPromocional(BigDecimal precoPromocional) {
+        this.precoPromocional = precoPromocional;
+    }
+
+    public String getClasseFiscal() {
+        return classeFiscal;
+    }
+
+    public void setClasseFiscal(String classeFiscal) {
+        this.classeFiscal = classeFiscal;
+    }
+
 }
