01fa3d812bc70d6512a7a446af81bda7883b9e31
Arthur <arthur.torres@oobj.com.br>
2022-05-05 17:11:19 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/easybill/easybill/EasybillApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.easybill.easybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybillApplication.class, args);
+	}
+
+}
